3df1142c728bbd19eec02786cac9471b16c20f28
arnaldqueiroga <arnaldqueiroga@gmail.com>
2020-09-30 10:25:01 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,30 +1,30 @@
 package com.devsuperior.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
+	// Declarando dependência do COntrolador para o Service	
+	@Autowired
+	private CategoryService service;
+	
 	@GetMapping // Para configurar que o método abaixo seja um web service, utilizamos a anotation @GetMapping
 	
 	// Criando o 1º End Point
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = new ArrayList<>(); // Instanciando a lista
-		
-		// Adicionando elementos na lista, add o novo objeto do meu tipo Category (o L maiúsculo serve
-		// para me lembrar que o Id da categoria é Long)
-		list.add(new Category (1L, "Books"));
-		list.add(new Category (2L, "Electonics"));
-		
+		List<Category> list = service.findAll(); // Essa lista chama o Service, que por sua vez chama o Repository, que por sua vez foi no BD, trouxe os objetos, os instanciou, e guardou nessa lista 
+				
 		// Mandar método FindAll retornar essa lista para o corpo da resposta HTTP dessa requisição
 		return ResponseEntity.ok().body(list);
 		
