0e95af848b15e68e48314c98de7016c8d381ef3f
arnaldqueiroga <arnaldqueiroga@gmail.com>
2020-10-05 00:34:01 -0300

    Product insert, update, delete

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -13,11 +13,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+
 
 
 
@@ -28,6 +32,11 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	 
 	
 	@Transactional(readOnly = true) // readOnly = true serve para evitarmos que façamos locking no banco, ou seja, não precisamos travar o banco somente para efetuar uma leitura
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -48,18 +57,21 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
+		copyDtoToEntity(dto, entity);
 		//entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 		
 	}
 	
+
+
 	// Criando o método update
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 			Product entity = repository.getOne(id);
-			//entity.setName(dto.getName());
+			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
 			return new ProductDTO(entity);		
 		}
@@ -86,6 +98,27 @@ public class ProductService {
 		}
 		
 	}	
+	
+	// Método auxiliar 	copyDtoToEntity
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		//Carregando categorias no dto para a entidade
+		entity.getCategories().clear(); // para limpar as categorias
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
+			
+			
+		}
+		
+		
+		
+	}
 	
 
 }
