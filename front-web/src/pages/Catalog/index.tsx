a3f7ec538eb0ca70146b1408b1823838d1ce5654
arnaldqueiroga <arnaldqueiroga@gmail.com>
2020-12-07 19:52:23 -0300

    Creating utility to make HTTP Request

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect } from "react"
 import { Link } from 'react-router-dom';
+import { makeRequest } from "../../core/utils/request";
 import ProductCard from "./components/ProductCard";
 import './styles.scss';
 
 const Catalog = () => {
 
-
+    // Quando o componente iniciar, buscar a lista de produtos
     useEffect( () => {
-        fetch('http://localhost:3000/products')
-        .then(response => response.json())
+        const params = {
+            page: 0,
+            linesPerPage: 12
+        }
+
+        makeRequest({url: '/products', params})        
         .then(response => console.log(response))
     } , []);
 
