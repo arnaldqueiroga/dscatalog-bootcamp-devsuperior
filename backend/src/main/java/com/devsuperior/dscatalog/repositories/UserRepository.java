d1d8ec86281867a19514c6ed68697d0609e78fac
arnaldqueiroga <arnaldqueiroga@gmail.com>
2021-01-05 15:30:40 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -10,5 +10,8 @@ import com.devsuperior.dscatalog.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	// Método que busque no BD um usuário pelo e-mail (esse método está sendo usado na classe UserInsertValidator)
+	User findByEmail (String email);	
 
 }
